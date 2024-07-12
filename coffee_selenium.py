a8d893a3a3c083af337f4d55363a02c2cdf13925
Edgar Santamaria <67978130+EdgarSantamaria@users.noreply.github.com>
2024-07-12 15:00:15 -0400

    added batch loading, size and price parsed
    - new batch loading using multi tabs
    - decreased waiting time per page
    - fixed scrolling, all items now load
    - added parsing for size and price
    - csv data updated with newest selection

--- coffee_selenium.py
+++ coffee_selenium.py
@@ -1,20 +1,24 @@
 from selenium_driverless import webdriver
+from selenium_driverless.types.by import By
 import asyncio
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 async def get_coffee_urls():
-    async with webdriver.Chrome() as browser:
+    options = webdriver.ChromeOptions()
+    async with webdriver.Chrome(options=options) as browser:
         # Open the selection page
         await browser.get("https://www.beanz.com/en-us/coffee")
+        # Wait for page to load
+        await asyncio.sleep(3)
 
         last_height = await browser.execute_script("return document.body.scrollHeight")
         while True:
-            # Scroll down to the bottom
-            await browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
+            # Scroll to above the footer to load coffees
+            await browser.execute_script(f"window.scrollTo(0, {last_height - 1300});")
             # Wait for new items to load
-            await asyncio.sleep(100)
+            await asyncio.sleep(1)
 
             # Calculate new scroll height and compare with last scroll height
             new_height = await browser.execute_script("return document.body.scrollHeight")
@@ -36,20 +40,11 @@ async def get_coffee_urls():
     coffee_urls = ["https://www.beanz.com" + link.get('href') for link in coffee_links]
     return coffee_urls
 
-
-async def scrape_coffee_page(url):
-    async with webdriver.Chrome() as browser:
-        # Open the coffee page
-        await browser.get(url)
-
-        # Wait for the page to load
-        await asyncio.sleep(3)
-
-        # Extract the page's full HTML
-        html = await browser.page_source
-
-        # Quit the driver
-        await browser.quit()
+# Function to scrape the coffee page for details
+async def scrape_coffee_page(target, url):
+    await target.get(url)
+    await asyncio.sleep(1)
+    html = await target.page_source
 
     # Parse the HTML
     soup = BeautifulSoup(html, 'html.parser')
@@ -66,29 +61,72 @@ async def scrape_coffee_page(url):
     notes_tag = soup.find('p', class_='xps-text xps-text-p2-bold product-info__tasting-notes')
     notes = notes_tag.get_text() if notes_tag else 'N/A'
 
-    # Extract country origin
+    # Extract bag details
     detail_tags = soup.find_all('p', class_='xps-text xps-text-p2-bold CoffeeInfo_roaster--detail__KLFjm')
     details = [tag.get_text() for tag in detail_tags] if detail_tags else ['N/A']
+
+    # Extract bag size and cost
+    bag_size_tag = soup.find('span', class_='xps-text xps-text-p3 option__text', string=lambda text: '|' in text)
+    # Bag size and cost are separated by '|', split the text
+    if bag_size_tag:
+        split = bag_size_tag.get_text().split('|')
+        if len(split) == 2:
+            size = split[0].strip()
+            price = split[1].strip()
+        else:
+            size = 'N/A'
+            price = 'N/A'
+    else:
+        size = 'N/A'
+        price = 'N/A'
 
     return {
         'vendor': vendor,
         'bag_name': bag_name,
         'notes': notes,
-        'details': details
+        'details': details,
+        'size': size,
+        'price': price
     }
+
+# For faster parsing, load multiple tabs at once
+async def batch_load_coffee_pages(coffee_urls, batch_size):
+    options = webdriver.ChromeOptions()
+    # Store coffee data
+    all_coffee_data = []
+    # Create a set to ensure no page is loaded twice
+    processed_urls = set()
+
+    # Loop through each coffee urls, according to batch size
+    for i in range(0, len(coffee_urls), batch_size):
+        # Get the batch that will be loaded
+        batch = coffee_urls[i:i + batch_size]
+
+        # Load the pages
+        async with webdriver.Chrome(options=options) as browser:
+            tasks = []
+            for url in batch:
+                if url not in processed_urls:
+                    # Scrape each coffee page
+                    target = await browser.new_window('tab', activate=False)
+                    tasks.append(scrape_coffee_page(target, url))
+                    processed_urls.add(url)
+
+            batch_data = await asyncio.gather(*tasks)
+            all_coffee_data.extend(batch_data)
+    return all_coffee_data
 
 
 async def main():
     # Get the URLs of individual coffee pages
     coffee_urls = await get_coffee_urls()
 
-    all_coffee_data = []
-    for url in coffee_urls:
-        # Scrape each coffee page
-        coffee_data = await scrape_coffee_page(url)
-        all_coffee_data.append(coffee_data)
+    # Test on a small batch
+    #coffee_urls = coffee_urls[:10]
+    # Send urls to batch loader function
+    coffee_data_list = await batch_load_coffee_pages(coffee_urls, batch_size=10)
 
-    return all_coffee_data
+    return coffee_data_list
 
 
 # Run the main function
